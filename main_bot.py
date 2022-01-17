95452e4e9541d9c51f15e85b8732955215ddfedc
SkarioT <vlad.skariot@gmail.com>
2022-01-17 21:17:58 +0300

    +nmap

--- main_bot.py
+++ main_bot.py
@@ -65,6 +65,23 @@ def nslookup(host='8.8.8.8',server='8.8.8.8'):
     print(response.decode('Windows-1251'))
     return response.decode('Windows-1251')
 
+def nmap(host='8.8.8.8'):
+    
+    p_sys = platform.system()
+    if p_sys == "Windows":
+        command = ['tracert', host]
+    else:
+        command = ['nmap', host]
+
+    try:
+        response = subprocess.check_output(command)
+    #добавить проверку статус кода \\ returned non-zero exit status 1
+    except subprocess.CalledProcessError as e:
+        response = e.output
+        # return response
+    print(response.decode('Windows-1251'))
+    return response.decode('Windows-1251')
+
 
 
 def telega_bot (token):
@@ -108,9 +125,18 @@ def telega_bot (token):
                 bot.send_message(message.chat.id,f"{nslookup(get_msg[1],get_msg[2])}")
             else:
                 bot.send_message(message.chat.id,f"{nslookup(get_msg[1])}")
+
+        elif str(message.text.lower()).startswith("/nmap"):
+            get_msg = str(message.text.lower()).split(" ")
+            print(get_msg)
+            if len(get_msg)==1:
+                bot.send_message(message.chat.id,f"Так а где смотрим порты то?")
+            else:
+                bot.send_message(message.chat.id,f"Выполняю команду {message.text.lower()}")
+                bot.send_message(message.chat.id,f"{nmap(get_msg[1])}")
         else:
             bot.send_message(message.chat.id,f"Чё надо, хозяин?")
-            bot.send_message(message.chat.id,f"Я умею делать трассировку:\n/tracert 8.8.8.8\nПинг:\n/ping 8.8.8.8\nNsLookUp:\n/nslookup host [ipdns,default 8.8.8.8]")
+            bot.send_message(message.chat.id,f"Я умею делать трассировку:\n/tracert 8.8.8.8\nПинг:\n/ping 8.8.8.8\nNsLookUp:\n/nslookup host [ipdns,default 8.8.8.8]\nnmap:\nnmap 8.8.8.8")
         
         
     bot.polling()
